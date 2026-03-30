46d1e953b5a841754faa9ae66dacca3e47e2d136
jorawaraksk <jorawarzu@proton.me>
2026-03-30 16:55:11 +0530

    Update configuration values in config.py
    Updated APP_ID and API_HASH with new values. Changed DB_URI to include a MongoDB connection string.

--- bot/config.py
+++ bot/config.py
@@ -9,8 +9,8 @@ logging.basicConfig(level=logging.INFO)
 LOGS = logging.getLogger(__name__)
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
+    APP_ID = config("16732227", cast=int)
+    API_HASH = config("8b5594ad7ad37f3a0a7ddbfb3963bb51")
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = config("DEV", default=5868426717, cast=int)
     OWNER = config("OWNER" , "5868426717")
@@ -18,7 +18,7 @@ try:
     THUMB = config("THUMBNAIL" , "https://graph.org/file/1cc8d7082dc910c0ccee8.jpg")
     
     # ADDED: Database configurations for the Restricted Content Login
-    DB_URI = config("DB_URI", default="")
+    DB_URI = config("DB_URI", default="mongodb+srv://zuzoo:Movie12345@cluster0.y7xfsuh.mongodb.net")
     DB_NAME = config("DB_NAME", default="vjsavecontentbot")
 except Exception as e:
     LOGS.info("Environment vars Missing")
